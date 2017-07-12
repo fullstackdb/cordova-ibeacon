77e9738a82a74504a56bd555dcbbf04a5c4df6a8
fullstackdb <a@web-ui.org>
2017-07-13 00:18:01 +0300

    add IBeacon plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IBeacon } from "@ionic-native/ibeacon";
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -9,14 +10,46 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private ibeacon: IBeacon) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-    });
-  }
+
+      // Request permission to use location on iOS
+      this.ibeacon.requestAlwaysAuthorization();
+      // create a new delegate and register it with the native layer
+      let delegate = this.ibeacon.Delegate();
+
+      // Subscribe to some of the delegate's event handlers
+      delegate.didRangeBeaconsInRegion()
+        .subscribe(
+        data => { console.log('didRangeBeaconsInRegion: ', data) },
+        error => { console.error(); });
+
+      delegate.didStartMonitoringForRegion()
+        .subscribe(
+        data =>  { console.log('didStartMonitoringForRegion: ', data) },
+        error => { console.error(); });
+
+      delegate.didEnterRegion()
+        .subscribe(
+        data => {
+          console.log('didEnterRegion: ', data);
+        });
+
+      let beaconRegion = this.ibeacon.BeaconRegion('testBeacon32', '2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6')
+      this.ibeacon.startMonitoringForRegion(beaconRegion)
+        .then(
+        () => console.log('Native layer recieved the request to monitoring'),
+        error => console.error('Native layer failed to begin monitoring: ', error)
+        );
+  });
+}
 }
