a0924b7f201a7d36dea1ca8615dba97fc867f8fe
vdzundza <victor.dzundza.dev@gmail.com>
2017-07-13 12:31:42 +0300

    fix

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,115 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.unarin.cordova.beacon.underscorejs",
+        "file": "plugins/com.unarin.cordova.beacon/www/lib/underscore-min-1.6.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "com.unarin.cordova.beacon.Q",
+        "file": "plugins/com.unarin.cordova.beacon/www/lib/q.min.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "com.unarin.cordova.beacon.LocationManager",
+        "file": "plugins/com.unarin.cordova.beacon/www/LocationManager.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "merges": [
+            "cordova.plugins"
+        ]
+    },
+    {
+        "id": "com.unarin.cordova.beacon.Delegate",
+        "file": "plugins/com.unarin.cordova.beacon/www/Delegate.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "com.unarin.cordova.beacon.Region",
+        "file": "plugins/com.unarin.cordova.beacon/www/model/Region.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "com.unarin.cordova.beacon.Regions",
+        "file": "plugins/com.unarin.cordova.beacon/www/Regions.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "com.unarin.cordova.beacon.CircularRegion",
+        "file": "plugins/com.unarin.cordova.beacon/www/model/CircularRegion.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "com.unarin.cordova.beacon.BeaconRegion",
+        "file": "plugins/com.unarin.cordova.beacon/www/model/BeaconRegion.js",
+        "pluginId": "com.unarin.cordova.beacon",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-console.console",
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "id": "cordova-plugin-console.logger",
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.unarin.cordova.beacon": "3.4.0",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-device": "1.1.6",
+    "cordova-plugin-splashscreen": "4.0.3",
+    "cordova-plugin-statusbar": "2.2.3",
+    "cordova-plugin-whitelist": "1.3.2",
+    "ionic-plugin-keyboard": "2.2.1"
+};
+// BOTTOM OF METADATA
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,74 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { IBeacon } from "@ionic-native/ibeacon";
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {IBeacon} from "@ionic-native/ibeacon";
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
 
-@Component({
-  templateUrl: 'app.html'
-})
+@Component({templateUrl: 'app.html'})
 export class MyApp {
-  rootPage: any = TabsPage;
+  rootPage : any = TabsPage;
 
-  constructor(platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    private ibeacon: IBeacon) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+  constructor(platform : Platform, statusBar : StatusBar, splashScreen : SplashScreen, private ibeacon : IBeacon) {
+    platform
+      .ready()
+      .then(() => {
+        // Okay, so the platform is ready and our plugins are available. Here you can do
+        // any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
 
-      // Request permission to use location on iOS
-      this.ibeacon.requestAlwaysAuthorization();
-      // create a new delegate and register it with the native layer
-      let delegate = this.ibeacon.Delegate();
+        // Request permission to use location on iOS
+        this
+          .ibeacon
+          .requestAlwaysAuthorization();
+        // create a new delegate and register it with the native layer
+        let delegate = this
+          .ibeacon
+          .Delegate();
 
-      // Subscribe to some of the delegate's event handlers
-      delegate.didRangeBeaconsInRegion()
-        .subscribe(
-        data => { console.log('didRangeBeaconsInRegion: ', data) },
-        error => { console.error(); });
+        // Subscribe to some of the delegate's event handlers
+        delegate
+          .didRangeBeaconsInRegion()
+          .subscribe(data => {
+            console.log('didRangeBeaconsInRegion: ', data)
+          }, error => {
+            console.error();
+          });
 
-      delegate.didStartMonitoringForRegion()
-        .subscribe(
-        data =>  { console.log('didStartMonitoringForRegion: ', data) },
-        error => { console.error(); });
+        delegate
+          .didStartMonitoringForRegion()
+          .subscribe(data => {
+            console.log('didStartMonitoringForRegion: ', data)
+          }, error => {
+            console.error();
+          });
 
-      delegate.didEnterRegion()
-        .subscribe(
-        data => {
-          console.log('didEnterRegion: ', data);
-        });
+        delegate
+          .didEnterRegion()
+          .subscribe(data => {
+            console.log('didEnterRegion: ', data);
+          });
 
-      let beaconRegion = this.ibeacon.BeaconRegion('testBeacon32', '2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6')
-      this.ibeacon.startMonitoringForRegion(beaconRegion)
-        .then(
-        () => console.log('Native layer recieved the request to monitoring'),
-        error => console.error('Native layer failed to begin monitoring: ', error)
-        );
-  });
-}
+        delegate
+          .didExitRegion()
+          .subscribe(data => {
+            console.log('didExitRegion: ', data);
+          });
+
+        // let ID = '7CDD91FB-F386-431A-B187-2F874390048C';
+        let ID = '2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6';
+        let beaconRegion = this
+          .ibeacon
+          .BeaconRegion('testBeacon32', ID, 3, 1)
+        this
+          .ibeacon
+          .startMonitoringForRegion(beaconRegion)
+          .then(() => console.log('Native layer recieved the request to monitoring'), error => console.error('Native layer failed to begin monitoring: ', error));
+
+        window['delegateTest'] = delegate;
+      });
+
+  }
 }
