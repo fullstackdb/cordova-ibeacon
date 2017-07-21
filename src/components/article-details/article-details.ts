2290e60a00958466856758dc2e7555593b4efbdc
alexsv7 <alexsv7@ukr.net>
2017-07-21 15:48:07 +0300

    fixed article detail view^ bsck button added

--- src/components/article-details/article-details.ts
+++ src/components/article-details/article-details.ts
@@ -1,23 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Article } from '../../models/article.interface';
 import { NavController, NavParams } from 'ionic-angular';
 
-//@IonicPage({
-//    name: 'article-detail',
-//    segment: 'article-detail/:id'
-//})
-
 @Component({
-  selector: 'article-details',
-  templateUrl: 'article-details.html'
+    selector   : 'article-details',
+    templateUrl: 'article-details.html'
 })
 export class ArticleDetailsComponent {
-   article: Article;
+    article: Article;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams){
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.article = this.navParams.data;
         console.log(this.navParams);
     }
 
-    //this.navParams.get('thing1');
+    goBack() {
+        this.navCtrl.pop();
+    }
 }
--- src/components/articles-list-item/articles-list-item.ts
+++ src/components/articles-list-item/articles-list-item.ts
@@ -1,17 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Article } from '../../models/article.interface';
-import { IonicPage, NavController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import {ArticleDetailsComponent} from '../article-details/article-details';
-
-//@IonicPage({
-//    name: 'articles-list-item'
-//})
 
 @Component({
     selector: 'articles-list-item',
     templateUrl: 'articles-list-item.html',
     entryComponents:[ ArticleDetailsComponent ]
-
 })
 export class ArticlesListItemComponent {
     @Input() article: Article;
@@ -19,11 +14,7 @@ export class ArticlesListItemComponent {
     constructor(public navCtrl: NavController) {}
 
     pushPage(article) {
-        // Push an `id` to the `'detail-page'`
         this.navCtrl.push(ArticleDetailsComponent, article);
-        console.log("navigattttted")
-
     }
-
 }
 
